7aaa98b70aec5ab4974877c13b1ae4aded3cfc58
Celso David <celunico43@gmail.com>
2024-04-19 23:13:41 -0300

    first component counter

---
+++ resources/views/livewire/counter.blade.php
@@ -0,0 +1,6 @@
+<div>
+    <h1>Counter</h1>
+    <button type="button" wire:click="decrement">-</button>
+    <h2>{{ $counter }}</h2>
+    <button type="button" wire:click="increment">+</button>
+</div>
